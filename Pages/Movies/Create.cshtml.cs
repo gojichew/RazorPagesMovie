5fb51d14f13f77d16e522595bd2ccf2e2de0fc89
william chu <willi0955@gmail.com>
2021-05-13 15:47:56 +0800

    first commit

---
+++ Pages/Movies/Create.cshtml.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using RazorPagesMovie.Data;
+using RazorPagesMovie.Models;
+
+namespace RazorPagesMovie.Pages.Movies
+{
+    public class CreateModel : PageModel
+    {
+        private readonly RazorPagesMovie.Data.RazorPagesMovieContext _context;
+
+        public CreateModel(RazorPagesMovie.Data.RazorPagesMovieContext context)
+        {
+            _context = context;
+        }
+
+        public IActionResult OnGet()
+        {
+            return Page();
+        }
+
+        [BindProperty]
+        public Movie Movie { get; set; }
+        public async Task<IActionResult> OnPostAsync()
+        {
+            var emptyMovie = new Movie();
+
+            if (await TryUpdateModelAsync<Movie>(
+                emptyMovie,
+                "Movie",   // Prefix for form value.
+                s => s.Title, s => s.ReleaseDate, s => s.Price, s => s.Genre))
+            {
+                _context.Movie.Add(emptyMovie);
+                await _context.SaveChangesAsync();
+                return RedirectToPage("./Index");
+            }
+
+            return Page();
+        }
+    }
+}
